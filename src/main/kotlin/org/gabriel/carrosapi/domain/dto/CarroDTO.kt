8f58c8e9b292d307c094e5dbcaf5c6934f7b161a
Gabriel Honda <gabrielhondacba@gmail.com>
2021-01-31 15:14:19 -0400

    feat: adiciona DTO da entidade Carro

---
+++ src/main/kotlin/org/gabriel/carrosapi/domain/dto/CarroDTO.kt
@@ -0,0 +1,11 @@
+package org.gabriel.carrosapi.domain.dto
+
+/**
+ * @project carros-api-kt
+ * @author daohn on 31/01/2021
+ */
+data class CarroDTO(
+  val id: Long,
+  val nome: String,
+  val tipo: String,
+)
