b6b256c484896583e37c4d08e3047e441c5dbd34
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-03 12:54:30 -0400

    feat: adiciona captura da exceção Method Not Allowed e retorna HTTP 405

--- src/main/kotlin/org/gabriel/carrosapi/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/org/gabriel/carrosapi/exceptions/ExceptionHandler.kt
@@ -1,18 +1,22 @@
 package org.gabriel.carrosapi.exceptions
 
 import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.lang.IllegalArgumentException
-import java.lang.IllegalStateException
+import org.springframework.web.context.request.ServletWebRequest
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 /**
  * @project carros-api-kt
  * @author daohn on 02/02/2021
  */
 @RestControllerAdvice
-class ExceptionHandler {
+class ExceptionHandler : ResponseEntityExceptionHandler() {
 
   @ExceptionHandler(EmptyResultDataAccessException::class)
   fun errorNotFound(exception: Exception): ResponseEntity<Any> {
@@ -22,5 +26,29 @@ class ExceptionHandler {
   @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class)
   fun errorBadRequest(exception: Exception): ResponseEntity<Any> {
     return ResponseEntity.badRequest().build()
+  }
+
+  override fun handleHttpRequestMethodNotSupported(
+    ex: HttpRequestMethodNotSupportedException,
+    headers: HttpHeaders,
+    status: HttpStatus,
+    request: WebRequest
+  ): ResponseEntity<Any> {
+
+    var message = "${ex.method} não é suportado para essa requisição. Os métodos suportados são: "
+
+    ex.supportedMethods?.forEach { method -> message += "$method " }
+
+    return ResponseEntity
+      .status(HttpStatus.METHOD_NOT_ALLOWED)
+      .body(
+        DefaultErrorMessage(
+          System.currentTimeMillis(),
+          status.value(),
+          status.name,
+          message,
+          (request as ServletWebRequest).request.requestURI,
+        )
+      )
   }
 }
