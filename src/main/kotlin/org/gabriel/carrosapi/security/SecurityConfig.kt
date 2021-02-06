12e267aa868821216afa64de4b204b9e9f559c6d
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-06 07:41:46 -0400

    feat: adiciona token JWT à configuração do Spring Security

--- src/main/kotlin/org/gabriel/carrosapi/security/SecurityConfig.kt
+++ src/main/kotlin/org/gabriel/carrosapi/security/SecurityConfig.kt
@@ -1,12 +1,17 @@
-package org.gabriel.carrosapi.config
+package org.gabriel.carrosapi.security
 
+import org.gabriel.carrosapi.security.handlers.AccessDeniedHandler
+import org.gabriel.carrosapi.security.handlers.UnauthorizedHandler
+import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
@@ -21,16 +26,25 @@ class SecurityConfig(
   @Qualifier("userDetailsService")
   val userDetailsService: UserDetailsService,
   val encoder: BCryptPasswordEncoder,
+  val jwtUtil: JWTUtil,
+  val modelMapper: ModelMapper,
+  val accessDeniedHandler: AccessDeniedHandler,
+  val unauthorizedHandler: UnauthorizedHandler,
 ) : WebSecurityConfigurerAdapter() {
 
   override fun configure(http: HttpSecurity) {
     http
       .authorizeRequests()
+      .antMatchers(HttpMethod.GET, "/api/v1/login").permitAll()
       .anyRequest().authenticated()
+      .and().csrf().disable()
+      .addFilter(JWTAuthenticationFilter(authenticationManager(), jwtUtil, modelMapper))
+      .addFilter(JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService))
+      .exceptionHandling()
+      .accessDeniedHandler(accessDeniedHandler)
+      .authenticationEntryPoint(unauthorizedHandler)
       .and()
-      .httpBasic()
-      .and()
-      .csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
   }
 
   override fun configure(auth: AuthenticationManagerBuilder) {
